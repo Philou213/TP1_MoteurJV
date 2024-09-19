e5390771e5b35d7f6cc57e59395cd2fb262cf4e1
Philippe St-Laurent-Recoura <71201023+Philou213@users.noreply.github.com>
2024-09-19 18:44:44 -0400

    add water effect layer, fix player diagonal movement, clean code

--- Scripts/player_character.gd
+++ Scripts/player_character.gd
@@ -3,22 +3,35 @@ extends CharacterBody2D
 @onready var animated_sprite_2d = $AnimatedSprite2D
 @export var SPEED = 2000
 
-func _ready():
-	animated_sprite_2d.play("Idle")
-
 func _physics_process(delta: float) -> void:
-	var horizontalDirection := Input.get_axis("left_axis", "right_axis")
-	var verticalDirection := Input.get_axis("top_axis", "down_axis")
-	var direction := Vector2(horizontalDirection,verticalDirection)
-	if (horizontalDirection < 0):
-		animated_sprite_2d.flip_h = true
-	if (horizontalDirection > 0):
-		animated_sprite_2d.flip_h = false
-	if (direction):
+	var direction := _getMovementDirection()
+	_flipSprite(direction.x)
+	_move(delta, direction)
+	
+
+func _move(delta: float, movement : Vector2) -> void:
+	if (movement):
+		velocity = movement * SPEED * delta
 		animated_sprite_2d.play("Moving")
-		velocity = direction * SPEED * delta
 	else:
 		velocity = velocity.move_toward(Vector2(0,0), SPEED * delta)
 		animated_sprite_2d.play("Idle")
-
 	move_and_slide()
+	
+func _flipSprite(horizontalMovement: float) -> void:
+	if horizontalMovement < 0:
+		animated_sprite_2d.flip_h = true
+	elif horizontalMovement > 0:
+		animated_sprite_2d.flip_h = false
+	
+func _getMovementDirection() -> Vector2:
+	var horizontalDirection := Input.get_axis("left_axis", "right_axis")
+	var verticalDirection := Input.get_axis("top_axis", "down_axis")
+	var direction := Vector2(horizontalDirection,verticalDirection)
+	
+	# Normalize the direction
+	if direction != Vector2.ZERO:
+		direction = direction.normalized()
+		
+	return direction
+	
