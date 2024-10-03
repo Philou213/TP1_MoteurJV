693a4d880b5b4c56d0d958d17a16abf5545126b4
Philippe St-Laurent-Recoura <71201023+Philou213@users.noreply.github.com>
2024-10-03 17:13:35 -0400

    add custom MainLoop

---
+++ Scripts/MasterManager.cs
@@ -0,0 +1,65 @@
+using Godot;
+using System;
+
+[GlobalClass]
+public partial class MasterManager : SceneTree
+{
+	private static MasterManager instance;
+	private static LevelManager levelManager;
+	private static SaveManager saveManager;
+	
+
+	public override void _Initialize()
+	{
+		base._Initialize();
+
+		GD.Print("Initialized:");
+
+		instance = new MasterManager();
+		
+	}
+
+	private void DeInitalize()
+	{
+		levelManager = null;
+		saveManager = null;
+		instance = null;
+	}
+
+	public static MasterManager GetMasterManager
+	{
+		get
+		{
+			if (instance == null)
+			{
+				instance = new MasterManager();
+			}
+			return instance;
+		}
+	}
+
+
+	public static LevelManager GetLevelManager
+	{
+		get
+		{
+			if (levelManager == null)
+			{
+				levelManager = new LevelManager();
+			}
+			return levelManager;
+		}
+	}
+
+	public static SaveManager GetSaveManager
+	{
+		get
+		{
+			if (saveManager == null)
+			{
+				saveManager = new SaveManager();
+			}
+			return saveManager;
+		}
+	}
+}
